5295b083c3b6f19319dee79b76d0c90cd7d7f2b0
demppas <dennis.staffansson@iths.se>
2024-08-12 23:05:01 +0200

    created implementation for EmployeeRepository with test class, added equals method and test in Employeeclass

--- src/test/java/com/example/EmployeesTest.java
+++ src/test/java/com/example/EmployeesTest.java
@@ -1,7 +1,26 @@
 package com.example;
 
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class EmployeesTest {
+    @Mock
+    private EmployeeRepository mockEmployeeRepository;
+    @Mock
+    private BankService mockBankService;
+
+
 
 }
